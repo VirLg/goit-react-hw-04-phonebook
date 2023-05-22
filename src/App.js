553005b7e2ddb88f9b731cc65a646e0e5556727d
VirLg <19virlg@gmail.com>
2023-05-22 14:39:49 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -15,13 +15,28 @@ import Form from './components/Form/Form';
 const App = () => {
   //*useState
   const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
   const [filter, setFilter] = useState('');
   //*useState
+
+  useEffect(() => {
+    if (contacts.length > 0)
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  useEffect(() => {
+    if (localStorage.contacts) {
+      const jsonContacts = localStorage.getItem('contacts');
+      const parseJson = JSON.parse(jsonContacts);
+      console.log(parseJson);
+      setContacts(parseJson);
+    }
+  }, []);
+
   //   const componentDidMount = () => {
   //     console.log('daadlfjkshd');
 
@@ -58,7 +73,7 @@ const App = () => {
       name,
       number,
     };
-    setContacts(prevState => [contact, ...prevState.contacts]);
+    setContacts(prevState => [contact, ...prevState]);
   };
 
   const DeleteContact = ContactId => {
